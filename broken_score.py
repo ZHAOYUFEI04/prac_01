9861bbf3ec909723eff25283f78a6f8eb2a0593c
ZHAOYUFEI04 <yufei.zhao1@my.jcu.edu.au>
2023-11-18 17:35:02 +0800

    Initial commit

---
+++ broken_score.py
@@ -0,0 +1,25 @@
+# # TODO: Fix this!
+# score = float(input("Enter score: "))
+# if score < 0:
+#     print("Invalid score")
+# else:
+#     if score > 100:
+#         print("Invalid score")
+#     if score > 50:
+#         print("Passable")
+#     if score > 90:
+#     print("Excellent")
+# if score < 50:
+#     print("Bad")
+
+score = float(input("Enter score: "))
+if 0 <= score <= 100:
+    if score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+else:
+    print("Invalid score")
+
